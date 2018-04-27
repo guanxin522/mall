f618e95c486a2bbade6fb12bd79130652ebcbe29
Guanxin Chen <guanxin.chen@foxmail.com>
2018-04-27 19:41:47 +0800

    initial

---
+++ src/page/common/index.js
@@ -0,0 +1,10 @@
+/*
+* @Author: xin
+* @Date:   2018-04-26 20:02:29
+* @Last Modified by:   xin
+* @Last Modified time: 2018-04-26 20:02:46
+*/
+
+
+
+console.log('i am global');
---
+++ src/page/module.js
@@ -0,0 +1,9 @@
+/*
+* @Author: xin
+* @Date:   2018-04-26 19:59:40
+* @Last Modified by:   xin
+* @Last Modified time: 2018-04-26 20:01:34
+*/
+module.exports = {
+    test:1234,
+}
